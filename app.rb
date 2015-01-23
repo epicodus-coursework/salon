4948ad5e88be352e344aabd1868415a12f733b2f
Matthew Terrill <meterrill@gmail.com>
2015-01-23 22:55:00 +0000

    Add index.erb and update app.rb

--- app.rb
+++ app.rb
@@ -6,3 +6,16 @@ require('./lib/stylist')
 require('pg')
 
 DB = PG.connect({:dbname => 'salon'})
+
+get("/") do
+  @stylists = Stylist.all()
+  erb(:index)
+end
+
+post("/stylists") do
+  stylist_name = params.fetch("stylist_name")
+  stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
